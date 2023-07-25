76920d1e43e15da709ffa9ab2e5eb1b6cf8cd0da
Teresita Jofre <tj.walburg@gmail.com>
2023-07-24 22:09:46 -0400

    /Tj/ - Agrega funcionalidad para eliminar estrellas - Agrega funcionalidad para transformar numeros a ordinales - Agrega funcionalidad para exportar explicacion

---
+++ test2/StarProcessor.py
@@ -0,0 +1,39 @@
+class StarProcessor(object):
+
+	originalPhrase = ''
+	explanations = ['Explanation: Performing the removal from left to right:']
+	finalPhrase = ''
+
+	def __init__(self, phrase):
+		self.originalPhrase = phrase
+		self.removeStars(phrase)
+		self.exposeExplanation()
+
+	def removeStars(self, phrase, n = 1):
+		ordinalNumber = self.getOrdinalString(n)
+
+		starPosition = phrase.find('*')
+		if (starPosition != -1):
+
+			phraseList = list(phrase)
+			phraseList.pop(starPosition)
+			letter = phraseList.pop(starPosition-1)
+
+			self.finalPhrase = ''.join(phraseList)
+			self.explanations.append(f"The closest character to the {ordinalNumber} star is '{letter}' in {phrase}, s becomes \"{self.finalPhrase}\"")
+			self.removeStars(self.finalPhrase, n+1)
+
+		if not phrase:
+			self.explanations.clear()
+			self.explanations.append('Explanation: The entire string is removed, se we return an empty string.')
+
+	def exposeExplanation(self):
+		print('Output: ', self.finalPhrase)
+
+		for explanation in self.explanations:
+			print('- ', explanation)
+
+	def getOrdinalString(self, n):
+		suffixes = { 1: "st", 2: "nd", 3: "rd" }
+		i = n if (n < 20) else (n % 10)
+		return str(n) + suffixes.get(i, 'th')
--- test2/main.py
+++ test2/main.py
@@ -1,18 +1,3 @@
-def removeStars(phrase, n = 1):
-	starPosition = phrase.find('*')
-	if (starPosition != -1):
-
-		phraseList = list(phrase)
-		phraseList.pop(starPosition)
-		phraseList.pop(starPosition-1)
-
-		newPhrase = ''.join(phraseList)
-		print(f"The closest character to the {n} star is  in {phraseList[starPosition - 1]}, s becomes \"{newPhrase}\"")
-		
-		removeStars(newPhrase, n+1)
-
-	return True
-
-
+from StarProcessor import StarProcessor
 phrase = input('Input: ')
-removeStars(phrase)
+StarProcessor(phrase)
